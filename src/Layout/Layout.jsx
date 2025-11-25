859f45e863725d4a06fee5003f91e9ae88d82fbb
João Victor Guimaraes <jvguimaraes2706@gmail.com>
2025-11-25 11:12:08 -0300

    arrumar

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,14 +1,14 @@
 export default function Layout({ children }) {
   return (
     <div style={{ padding: 10 }}>
-      <header style={{ background: "black", padding: 10, borderRadius: 10, textAlign: "center" }}>
-        <h2 style={{color: "white"}}>Nome do Site</h2>
+      <header style={{ background: "#ffd700", padding: 20, borderRadius: 10, textAlign: "justify" }}>
+        <h2 style={{color: "black"}}>Nome do Site</h2>
       </header>
 
       <main>{children}</main>
 
-      <footer style={{ background: "black", padding: 10, marginTop: 10, borderRadius: 10 }}>
-        <p  style={{color: "white", textAlign: "center" }}>Colocar as informações do site - Footer.</p>
+      <footer style={{ background: "#ffd700", padding: 10, marginTop: 10, borderRadius: 10 }}>
+        <p  style={{color: "black", textAlign: "center" }}>Colocar as informações do site - Footer.</p>
       </footer>
     </div>
   );
